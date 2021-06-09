7cd7ae369df15ca4693f556ecf410bc17b1d93e3
ZJianJi <1774031367@qq.com>
2021-06-09 17:39:11 +0800

    系统初始布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div calss="home-cantainer">
+    <div class="right-container"
+         :class="{ buttonLeft: collapsed }"
+    >
+      <div class="header-container">
+        <!-- 菜单栏按钮 -->
+        <a-button
+          type="primary"
+          style="margin-bottom: 16px"
+          @click="toggleCollapsed"
+        >
+          <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <!-- 面包屑 -->
+        <a-breadcrumb>
+          <a-breadcrumb-item>商品</a-breadcrumb-item>
+          <a-breadcrumb-item
+            ><a href="">商品列表</a></a-breadcrumb-item
+          >
+        </a-breadcrumb>
+        <ul class="user">
+          <li>小朱，你好！<a-icon type="down" /></li>
+          <li>退出</li>
+        </ul>
+      </div>
+    </div>
+    <a-menu
+      :default-selected-keys="['1']"
+      :default-open-keys="['sub1']"
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="collapsed"
+      :class="{ fold: collapsed }"
+    >
+      <a-menu-item key="1">
+        <a-icon type="pie-chart" />
+        <span>Option 1</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <a-icon type="desktop" />
+        <span>Option 2</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <a-icon type="inbox" />
+        <span>Option 3</span>
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <span slot="title"
+          ><a-icon type="mail" /><span>Navigation One</span></span
+        >
+        <a-menu-item key="5"> Option 5 </a-menu-item>
+        <a-menu-item key="6"> Option 6 </a-menu-item>
+        <a-menu-item key="7"> Option 7 </a-menu-item>
+        <a-menu-item key="8"> Option 8 </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <span slot="title"
+          ><a-icon type="appstore" /><span>Navigation Two</span></span
+        >
+        <a-menu-item key="9"> Option 9 </a-menu-item>
+        <a-menu-item key="10"> Option 10 </a-menu-item>
+        <a-sub-menu key="sub3" title="Submenu">
+          <a-menu-item key="11"> Option 11 </a-menu-item>
+          <a-menu-item key="12"> Option 12 </a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+    </a-menu>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
 </script>
+
+<style scoped lang='less'>
+@import "~@/assets/css/home.less";
+</style>
